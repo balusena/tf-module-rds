cf326aa64c64f5f1898b2c5a7070ed693d883b4e
balusena <balusena12@gmail.com>
2024-01-13 22:24:33 +0000

    roboshop terraform files

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
 resource "aws_db_subnet_group" "main" {
   name                    = "main"
   subnet_ids              = var.subnet_ids
-
   tags                    = merge({ Name = "${var.component}-${var.env}" }, var.tags)
 }
 
-resource "aws_rds_cluster" "default" {
+resource "aws_rds_cluster" "main" {
   cluster_identifier      = "${var.component}-${var.env}"
   engine                  = var.engine
   engine_version          = var.engine_version
